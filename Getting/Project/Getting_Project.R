e741f0bdcedd3a33bbdffae903372736d195ae26
dhimrich <davehimrich@yahoo.com>
2014-09-20 10:14:26 -0500

    Initial commit of script for Getting and Cleaning Data course project.

---
+++ Getting/Project/Getting_Project.R
@@ -0,0 +1,13 @@
+# Getting and Cleaning Data, Course Project
+# 
+# Place the following files from the uCI HAR data set in the working directory:
+#   X_train.txt
+#   y_train.txt
+#   subject_train.txt
+#   X_test.txt
+#   y_test.txt
+#   subject_test.txt
+#   features.txt
+#   activity_labels.txt
+#
+
