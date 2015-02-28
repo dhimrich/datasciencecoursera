c105f8f8655d9889a53aabd69827abed2462c5bb
dhimrich <davehimrich@yahoo.com>
2015-02-28 14:01:50 -0600

    Shiny ui.R Changed per Lesson 3 instructions

--- Shiny Tutorial/App-1/ui.R
+++ Shiny Tutorial/App-1/ui.R
@@ -3,33 +3,25 @@ library(shiny)
 # ui.R
 
 shinyUI(fluidPage(
-  titlePanel("My Shiny App"),
+  titlePanel("censusVis"),
+  
   sidebarLayout(
     sidebarPanel(
-      h2("Installation"),
-      p("Shiny is available on CRAN, so you can install it in the usual
-way from your R console:"),
-      code("install.packages(\"shiny\")"),
-      br(),
-      br(),
-      br(),
-      br(),
-      img(src="BigOrb.png", height = 72, width = 72,
-      "shiny is a product of", span("RStudio", style = "color:blue"))
-    ),
-    mainPanel(
-      h1("Introducing Shiny"),
-      p("Shiny is a new package from RStudio that makes it",
-        em("incredibly easy"), " to build interactive web applications with R."),
-      br(),
-      p("For an introduction and live examples, vist the"),
-      a("Shiny homepage", href="http://www.rstudio.com/shiny"),
-      h2("Features"),
-      p("* Build useful web with only a few lines of code - no JavaScript
-        required"),
-      p("* Shiny applications are automatically \"live\" in the same way that",
-        strong("spreadsheets")," are live. Outputs change instantly as users modify
-        inputs, without requiring a reload of the browser.")
-    )
+      helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = list("Percent White", "Percent Black",
+                                 "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+      ),
+    
+    mainPanel()
   )
 ))
+
